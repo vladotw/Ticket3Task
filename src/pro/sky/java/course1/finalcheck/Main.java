57edf4418333e20909b0115e8f314a8a38a71667
Vlad <vladot@ya.ru>
2022-12-28 19:14:17 +0600

    Задача 3 билета

--- src/pro/sky/java/course1/finalcheck/Main.java
+++ src/pro/sky/java/course1/finalcheck/Main.java
@@ -1,9 +1,25 @@
 package pro.sky.java.course1.finalcheck;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
 
+        System.out.println("Введите строку: ");
+
+        String str = in.nextLine();
+        boolean strIsEmpty = str.isEmpty();
+        int mid = str.length() / 2;
+
+        if (!strIsEmpty) {
+            String firstPart = str.substring(0, mid);
+            String secondPart = str.substring(mid);
+
+            System.out.println(firstPart + " | " + secondPart);
+        } else {
+            System.out.println("Строка не должна быть пустой!");
+        }
 
     }
 }
